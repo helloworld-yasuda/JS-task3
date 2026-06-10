68337cdaa64d7c691310b6243b5af1b926982c9b
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-06-10 14:45:41 +0900

    一つの関数にまとめました、同じように正規表現に修正しました

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const user = '{"name":"Taro", "age":42, "gender": "male"}';
 const userObj = JSON.parse(user);
 console.log(userObj);
 
-const User = { name: "Taro", Age: 13, gender: "male" };
-const JSONFormat = JSON.stringify(User);
+const profile = { name: "Taro", Age: 13, gender: "male" };
+const JSONFormat = JSON.stringify(profile);
 console.log(JSONFormat);
 
 const validationCheck = (targetId, regex, errorId, errorMessage)=>{
@@ -19,14 +19,17 @@ const newBtn = document.getElementById("submitBtn");
 newBtn.addEventListener("click",()=>{
     const textClear = document.querySelectorAll(".error");
     textClear.forEach(src => src.textContent = "");
-
+    const validationCheck = (targetId, regex, errorId, errorMessage)=>{
+    const targetText = document.getElementById(targetId).value;
+    const checkRegexError = !regex.test(targetText);
+    if(targetText === "" || checkRegexError){
+        const targetError = document.getElementById(errorId);
+        targetError.textContent = errorMessage;
+        }
+    }
+    
     validationCheck("nameInput",  /^[^\x01-\x7E\uFF61-\uFF9F]+$/, "nameError",  "正しい名前を入力してください");
     validationCheck("ageInput", /^[0-9]+$/, "ageError", "正しい年齢を入力してください");
     validationCheck("emailInput", /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/, "emailError", "正しいメールアドレスを入力してください");
-
-    const phoneText = document.getElementById("phoneInput").value;
-    if(phoneText === "" || phoneText.length !== 11 || isNaN(phoneText)){
-        const phoneCheck = document.getElementById("phoneError");
-        phoneCheck.textContent = "正しい電話番号を入力してください"
-    }
+    validationCheck("phoneInput",  /^[0-9]{11}$/, "phoneError", "正しい電話番号を入力してください");
 });
